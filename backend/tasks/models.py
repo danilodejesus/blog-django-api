628baea6342334f67224141c4bd309fe1789f60f
Danilo Viacava <daniloviacava.acidlab@latam.com>
2024-10-23 20:26:19 +0200

    add model blog

--- backend/tasks/models.py
+++ backend/tasks/models.py
@@ -3,6 +3,10 @@ from django.db import models
 # Create your models here.
 class Task(models.Model):
   title = models.CharField(max_length=100)
+  country = models.CharField(max_length=100)
+  price = models.CharField(max_length=10)
+  date_tour = models.CharField(max_length=50)
+  image = models.ImageField(upload_to='uploads/')
   description = models.TextField(null=True, blank=True)
   done = models.BooleanField(default=False)
   created_at = models.DateTimeField(auto_now_add=True)
--- backend/tasks/serializers.py
+++ backend/tasks/serializers.py
@@ -4,5 +4,5 @@ from .models import Task
 class TaskSerializer(serializers.ModelSerializer):
   class Meta:
     model = Task
-    fields = ('id', 'title', 'description', 'done', 'created_at')
+    fields = ('id', 'title', 'country', 'price', 'date_tour', 'image', 'description', 'done', 'created_at')
     read_only_fields = ('id', 'created_at')
